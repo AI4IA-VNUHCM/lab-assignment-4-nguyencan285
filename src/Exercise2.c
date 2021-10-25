633170bea2cd3f261480d8f85c565a866a2001d8
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-10-25 22:33:29 +0700

    1

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -51,9 +51,9 @@ void printArray(int a[SIZE][SIZE], int m, int n)
 void down(int a[SIZE][SIZE],int m,int n)
 {
 	int i,j;
-	for (i=0;i<=m-1;i++)
+	for (j=0;j<=n-1;j++)
 	{
-		for (j=0;j<=n-1;j++)
+		for (i=0;i<=m-1;i++)
 		{
 			if( j % 2==0)
 			{
@@ -71,9 +71,9 @@ void down(int a[SIZE][SIZE],int m,int n)
 void up(int a[SIZE][SIZE],int m,int n)
 {
 	int i,j;
-	for (i=0;i<=m-1;i++)
+	for (j=0;j<=n-1;j++)
 	{
-		for (j=0;j<=n-1;j++)
+		for (i=0;i<=m-1;i++)
 		{
 			if( j % 2!=0)
 			{
