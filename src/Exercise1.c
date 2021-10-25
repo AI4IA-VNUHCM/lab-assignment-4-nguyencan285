43d346d0174a8eaf95e39ec1dbedfb6fb7abbaf7
nguyencan285 <91868096+nguyencan285@users.noreply.github.com>
2021-10-25 15:15:42 +0700

    2

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 int i,j;
 	int arr[size][size];
 	testcase= testcase +1;
-	for(i=0;i<n;i++){
+	for(i=0;i<testcase;i++){
 		for(j=0;i<i;j++){
 			if(j==0||j==i)
 			arr[i][j]=1;
